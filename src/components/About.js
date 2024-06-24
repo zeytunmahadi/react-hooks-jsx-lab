96916e8aa27496753cf2bd443f13a6a655f0d625
zeitun <zeytunmahadi718@gmail.com>
2024-06-24 20:32:06 +0300

    syntax to create DOM elements

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>my name is zeytun</p>
+    <img src={ image } alt="I made this" />
+  </div>;
 }
 
 export default About;
